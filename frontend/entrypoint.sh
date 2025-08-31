322fcad1aa26aeec6c37c89b0b88332043e9a80c
Dmytro Levchenko <d.levchenko@wezom.com>
2025-09-01 00:56:13 +0300

    fix entrypoint.sh

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,8 +2,9 @@
 set -e
 
 : "${BACKEND_URL:?Env BACKEND_URL is required}"
+export PORT="${PORT:-8080}"
 
-envsubst '${BACKEND_URL}' \
+envsubst '${BACKEND_URL} ${PORT}' \
   < /etc/nginx/conf.d/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
