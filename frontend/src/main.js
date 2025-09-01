320840543e882d56ad2f38978e1eadf105d23019
Dmytro Levchenko <d.levchenko@wezom.com>
2025-09-01 10:40:58 +0300

    fix imports in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { DefaultApolloClient } from '@vue/apollo-composable'
-import { apolloClient } from './graphql/client'
+import apolloClient from './graphql/client'
 import App from './App.vue'
 import './style.css'
 
