520fa7770752eb186457e69bdf015e803a7b41a7
Dmytro Levchenko <d.levchenko@wezom.com>
2025-09-01 19:39:40 +0300

    change client.js

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -1,18 +1,13 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 import { createUploadLink } from 'apollo-upload-client';
 
-const backendBase =
-  (window.__CONFIG__ && window.__CONFIG__.API_BASE) ||
-  import.meta.env.VITE_API_BASE_URL ||
-  '';
+const backendBase = ((window.__CONFIG__?.API_BASE || import.meta.env.VITE_API_BASE_URL || '').replace(/\/$/, '')
 
-const graphqlURL =
-  (window.__CONFIG__ && window.__CONFIG__.GRAPHQL_URL) ||
-  (backendBase ? `${backendBase.replace(/\/$/, '')}/graphql/` : '/graphql/');
-
+const graphqlURL = window.__CONFIG__?.GRAPHQL_URL || (backendBase ? `${backendBase}/graphql/` : '/graphql/')
 
 const link = createUploadLink({
   uri: graphqlURL,
+  credentials: 'include',
   fetchOptions: { credentials: 'include' },
 });
 
