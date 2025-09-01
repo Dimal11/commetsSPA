3e2e780bd7fdcaa64ce97b6c53a1feea062fa5e4
Dmytro Levchenko <d.levchenko@wezom.com>
2025-09-01 10:29:09 +0300

    fix client.js

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -1,35 +1,22 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client/core'
-import { createUploadLink } from 'apollo-upload-client'
-import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
-import { createClient } from 'graphql-ws'
-import { getMainDefinition } from '@apollo/client/utilities'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+import { createUploadLink } from 'apollo-upload-client';
 
-const API = (import.meta.env.VITE_API_BASE_URL ?? '').replace(/\/$/,'')
+const backendBase =
+  (window.__CONFIG__ && window.__CONFIG__.API_BASE) ||
+  import.meta.env.VITE_API_BASE_URL ||
+  '';
 
-const httpUri = API ? `${API}/graphql/` : '/graphql/'
-const httpLink = createUploadLink({
-  uri: httpUri,
+const graphqlURL =
+  (window.__CONFIG__ && window.__CONFIG__.GRAPHQL_URL) ||
+  (backendBase ? `${backendBase.replace(/\/$/, '')}/graphql/` : '/graphql/');
+
+const link = createUploadLink({
+  uri: graphqlURL,
   fetchOptions: { credentials: 'include' },
-})
-
-const wsUrl = API.startsWith('https')
-  ? API.replace(/^https?/, 'wss') + '/graphql/'
-  : API.replace(/^http/, 'ws') + '/graphql/'
-const wsLink = new GraphQLWsLink(createClient({ url: wsUrl }))
-
-const backendBase = window.__CONFIG__?.API_BASE || import.meta.env.VITE_API_BASE_URL || '';
-
-const graphqlURL = window.__CONFIG__?.GRAPHQL_URL
-  || (backendBase ? `${backendBase.replace(/\/$/, '')}/graphql` : '')
-  || 'http://127.0.0.1:8000/graphql';
-
-const link = createHttpLink({
-  uri: graphqlURL.replace(/\/$/, ''),
-  credentials: 'include',
 });
 
 export default new ApolloClient({
   link,
   cache: new InMemoryCache(),
-  connectToDevTools: true,
+  connectToDevTools: import.meta.env.DEV,
 });
