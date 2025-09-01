6bee7c4ef431ef0c5cc6c6a00fc187e6465fe08f
Dmytro Levchenko <d.levchenko@wezom.com>
2025-09-01 17:40:02 +0300

    fix url in config.js

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,4 +1,4 @@
 window.__CONFIG__ = {
-  API_BASE: "https://comments-backend-755819237934.europe-central2.run.app/api/",
-  GRAPHQL_URL: "https://comments-backend-755819237934.europe-central2.run.app/graphql/"
+  API_BASE: "https://comments-backend-755819237934.europe-central2.run.app",
+//  GRAPHQL_URL: "https://comments-backend-755819237934.europe-central2.run.app/graphql/"
 };
